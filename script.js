07736dcf8eccfe1e46695cd74300b48e577bb92b
JoeL <pogpland@aol.com>
2022-08-08 11:29:35 -1000

    inputcolors

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ var saveButton9 = document.getElementById("button-addon9")
 
 //Current date variables
 var today = new Date();
+var hours = today.getHours();
 var day = today.getDate();
 var month = today.getMonth() + 1;
 var year = today.getFullYear();
@@ -37,7 +38,124 @@ h2El.textContent = currentDate
 //appending the current date to the header
 parent.appendChild(h2El)
 
+console.log(hours);
 
+//changes color of 9am input depending on if past, present, future
+if (input1) {
+    if (hours < 9) {
+        input1.style.backgroundColor = "green";
+    }
+    else if (hours == 9) {
+        input1.style.backgroundColor = "red";
+    }
+    else (hours > 9) ;{
+        input1.style.backgroundColor = "gray";
+    }
+};
+
+//changes color of 10am input depending on if past, present, future
+if (input2) {
+    if (hours < 10) {
+        input2.style.backgroundColor = "green";
+    }
+    else if (hours == 10) {
+        input2.style.backgroundColor = "red";
+    }
+    else (hours > 10) ;{
+        input2.style.backgroundColor = "gray";
+    }
+};
+
+//changes color of 11am input depending on if past, present, future
+if (input3) {
+    if (hours < 11) {
+        input3.style.backgroundColor = "green";
+    }
+    else if (hours == 11) {
+        input3.style.backgroundColor = "red";
+    }
+    else (hours > 11) ;{
+        input3.style.backgroundColor = "gray";
+    }
+};
+
+//changes color of 12pm input depending on if past, present, future
+if (input4) {
+    if (hours < 12) {
+        input4.style.backgroundColor = "green";
+    }
+    else if (hours == 12) {
+        input4.style.backgroundColor = "red";
+    }
+    else (hours > 12) ;{
+        input4.style.backgroundColor = "gray";
+    }
+};
+
+//changes color of 1pm input depending on if past, present, future
+if (input5) {
+    if (hours < 13) {
+        input5.style.backgroundColor = "green";
+    }
+    else if (hours == 13) {
+        input5.style.backgroundColor = "red";
+    }
+    else (hours > 13) ;{
+        input5.style.backgroundColor = "gray";
+    }
+};
+
+//changes color of 2pm input depending on if past, present, future
+if (input6) {
+    if (hours < 14) {
+        input6.style.backgroundColor = "green";
+    }
+    else if (hours == 14) {
+        input6.style.backgroundColor = "red";
+    }
+    else (hours > 14) ;{
+        input6.style.backgroundColor = "gray";
+    }
+};
+
+//changes color of 3pm input depending on if past, present, future
+if (input7) {
+    if (hours < 15) {
+        input7.style.backgroundColor = "green";
+    }
+    else if (hours == 15) {
+        input7.style.backgroundColor = "red";
+    }
+    else (hours > 15) ;{
+        input7.style.backgroundColor = "gray";
+    }
+};
+
+//changes color of 4pm input depending on if past, present, future
+if (input8) {
+    if (hours < 16) {
+        input8.style.backgroundColor = "green";
+    }
+    else if (hours == 16) {
+        input8.style.backgroundColor = "red";
+    }
+    else (hours > 16) ;{
+        input8.style.backgroundColor = "gray";
+    }
+};
+
+//changes color of 5pm input depending on if past, present, future
+if (input9) {
+    if (hours < 17) {
+        input9.style.backgroundColor = "green";
+    }
+    else if (hours == 17) {
+        input9.style.backgroundColor = "red";
+    }
+    else (hours > 17) ;{
+        input9.style.backgroundColor = "gray";
+    }
+};
 
 //button 1 event click function
 saveButton1.addEventListener("click", function(event) {
