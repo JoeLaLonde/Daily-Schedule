7f2d63d643835bacb6ea31db8f9e616020d50b65
JoeL <pogpland@aol.com>
2022-08-08 15:07:25 -0400

    add

---
+++ script.js
@@ -0,0 +1,67 @@
+//All Input variables
+var input1 = document.getElementById("input1")
+var input2 = document.getElementById("input2")
+var input3 = document.getElementById("input3")
+var input4 = document.getElementById("input4")
+var input5 = document.getElementById("input5")
+var input6 = document.getElementById("input6")
+var input7 = document.getElementById("input7")
+var input8 = document.getElementById("input8")
+var input9 = document.getElementById("input9")
+
+//all button variables
+var saveButton1 = document.getElementById("button-addon1")
+var saveButton2 = document.getElementById("button-addon2")
+var saveButton3 = document.getElementById("button-addon3")
+var saveButton4 = document.getElementById("button-addon4")
+var saveButton5 = document.getElementById("button-addon5")
+var saveButton6 = document.getElementById("button-addon6")
+var saveButton7 = document.getElementById("button-addon7")
+var saveButton8 = document.getElementById("button-addon8")
+var saveButton9 = document.getElementById("button-addon9")
+
+
+saveButton1.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("click")
+});
+
+saveButton2.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("click")
+});
+
+saveButton3.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("click")
+});
+
+saveButton4.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("click")
+});
+
+saveButton5.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("click")
+});
+
+saveButton6.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("click")
+});
+
+saveButton7.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("click")
+});
+
+saveButton8.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("click")
+});
+
+saveButton9.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("click")
+});
